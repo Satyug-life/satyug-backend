db130f56c3001f2a7cec05a63abd5fbea9559ff1
Rupin Vijan <87141274+RupinVijan@users.noreply.github.com>
2022-12-12 20:33:41 +0530

    Update RamSetuContactController.js

--- src/controllers/RamSetuContactController.js
+++ src/controllers/RamSetuContactController.js
@@ -22,7 +22,7 @@ exports.postContactData = async (req,res) => {
             res.status(200).json({status:false ,users, msg: "User Already Exist" , userExist:true });
         }
 		const users = await RamSetuContactModel.create(req.body);
-		res.status(200).json({status:false ,status:true ,users, msg: "created successfully" });
+		res.status(200).json({status:true ,users, msg: "created successfully" });
 	}
 	catch(err)
 	{
